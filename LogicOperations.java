497ea2f0d9105359f0652c55b63e524248b2c0ac
JavaPsyh <psyhjava@gmail.com>
2019-05-31 15:33:50 +0300

    changes from TA comments

---
+++ LogicOperations.java
@@ -0,0 +1,8 @@
+package psyh;
+
+public class LogicOperations {
+
+    public static boolean LogicOperations(boolean a, boolean b, boolean c, boolean d){
+        return (a ^ b) && (c ^ d) || (a ^ c) && (b ^ d);
+    }
+}
